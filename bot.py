6b82f5a9c16a67e212c85d6c4ce9e9d3798f75b0
Scott-Fischer <scottfischer892@gmail.com>
2022-07-29 12:44:30 -0700

    Updated Database tracking to account for clocking out before clocking in. Also added motivational quotes

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #------------------------------------------#
 #   Created by Scott Fischer
 #
-#   This is a project that clocks user activity and stores it in a dataBase
+#   This is a project that clocks user activity and stores it in a SQL dataBase
 #   
 #------------------------------------------#
 
@@ -9,14 +9,16 @@
 import os
 import discord
 import sqlite3
+import random
 from dateutil import tz
 from dotenv import load_dotenv
+from enum import Enum
 
-
+#Constants
 from_zone = tz.tzutc()
 to_zone = tz.tzlocal()
 
-
+#-----------Setting Up Bot------------
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -36,8 +38,14 @@ try:
                  (id Text, start TEXT, end TEXT, total REAL, pto REAL)''')
 
 except Exception as e:
-    print("Table already exists" + str(e))
+    print("Error in making table: " + str(e) + "\n")
 #--------------------------------------
+
+#Read from the prompts txt and fill an array with quotes
+prompts = open("prompts.txt",'r')
+
+Quotes = prompts.readlines()
+#-------------------------------------------------------
 
 async def parse_message(message):
     time = ''
@@ -48,52 +56,76 @@ async def parse_message(message):
     print(time)
 
 async def add_clock_in(user,time_in):
-    print("TIME IN: " + time_in)
-    hours = time_in[0:2]
-    mins  = time_in[2:4]
-    secs  = time_in[4:6]
-    print("Hours: " + hours + " Mins: " + mins + " Secs: " + secs)
-    
-    #We query 
-    query = curr.execute('select exists (select 1 from timecards where id=? collate nocase) limit 1',(user,))
+    #We query if user exists
+    q = 'select exists (select 1 from timecards where id=? collate nocase) limit 1'
+    query = curr.execute(q,(user,))
+
     if query.fetchone()[0] == 1:
-        #Exists
-        #conn.execute('INSERT INTO timecards (id,start) VALUES(?,?)',(user,time_in))
-        conn.execute('UPDATE timecards SET start=?  WHERE id=? collate nocase',(time_in,user))
+        #Exists so we Update the DB instead of inserting
+        conn.execute('''UPDATE timecards SET start=?  
+                    WHERE id=? collate nocase'''\
+                    ,(time_in,user))
 
     else:
-        #Does not exist
-        conn.execute('INSERT INTO timecards (id,start,total,pto) VALUES(?,?,?,?)',(user,time_in,0,0))
+        #Does not exist so we insert new employee id
+        conn.execute('''INSERT INTO timecards 
+                    (id,start,total,pto) 
+                    VALUES(?,?,?,?)'''\
+                    ,(user,time_in,0,0))
 
     conn.commit()
 
-async def clock_out(user, time_out):
-    #time_out = input("GIVE A  TIME PLS: ")
+def clock_out(user, time_out):
+    
     #We want to convert the times to ints and find the difference
-    time_in = curr.execute('select start from timecards where id=? collate nocase',(user,)).fetchone()[0]
-    old_PTO = curr.execute('select pto from timecards where id=? collate nocase',(user,)).fetchone()[0]
-    old_total = curr.execute('select total from timecards where id=? collate nocase',(user,)).fetchone()[0]
+    time_in = curr.execute('select start from timecards where id=? collate nocase'\
+                            ,(user,)).fetchone()[0]
+    old_PTO = curr.execute('select pto from timecards where id=? collate nocase'\
+                            ,(user,)).fetchone()[0]
+    old_total = curr.execute('select total from timecards where id=? collate nocase'\
+                            ,(user,)).fetchone()[0]
+    if time_in == '-1':
+        #print('Must clock in before clocking out')
+        return (0.0,0.0)
 
+    #Extract hours, mins, and seconds from strings
     hours = abs(int(time_out[0:2]) - int(time_in[0:2]))
     mins  = abs(int(time_out[2:4]) - int(time_in[2:4]))
     secs  = abs(int(time_out[4:6]) - int(time_in[4:6]))
 
+    #Calculate time in hours
     total_hours = hours + (mins/60) + (secs/3600)
-    #print("OUT : " + time_out)
-    #print("WORKED FOR: " + str(hours) + " mins: " + str(mins) + " secs: " + str(secs))
-
+    
+    #Calculate PTO using 0.062 as ratio of PTO
     earned_PTO = (hours * 0.062) + (mins/60 * 0.062) + (secs/3600 * 0.062)
     new_PTO = earned_PTO + old_PTO
     
     new_total = old_total + total_hours
-
-    query = curr.execute('select exists (select 1 from timecards where id=? collate nocase) limit 1',(user,))
+    q = 'select exists (select 1 from timecards where id=? collate nocase) limit 1'
+    query = curr.execute(q,(user,))
     if query.fetchone()[0] == 1:
-        #Exists
-        #curr.execute('INSERT INTO timecards (user,end,total,pto) VALUES(?,?,?,?)',(user,time_out,))
-        curr.execute('''UPDATE timecards SET end=?,total=?,pto=? 
-                     WHERE id=? collate nocase''',(time_out,new_total,new_PTO,user)) 
+        #User exists so we update Database
+        curr.execute('''UPDATE timecards SET start=?,end=?,total=?,pto=? 
+                     WHERE id=? collate nocase'''\
+                     ,('-1',time_out,new_total,new_PTO,user)) 
+    
     conn.commit()
+    return (earned_PTO,new_PTO)
+
+def get_times(message):
+    #Get time from the message
+    datetime = message.created_at
+    
+    #Convert UTC time to String
+    utc_time = datetime.strftime('%H%M%S')
+    
+    #Tell UTC time it is in UTC bc it is niave to begin
+    local = datetime.replace(tzinfo=from_zone)
+    
+    #Convert UTC to local time and get string
+    loc_time = local.astimezone(to_zone).strftime("%I:%M:%S %p")
+
+    return (utc_time,loc_time)
 
 @client.event
 async def on_ready():
@@ -108,40 +140,54 @@ async def on_message(message):
         await parse_message(message_low)
         
         user = message.author.name
-
-        #Get time the message was sent (This is given in UTC time)
-        datetime = message.created_at
-
-        #We format the datetime object to formatted string
-        utc_time = datetime.strftime('%H%M%S')
-
-        #Now we want to convert to local time
-        #The datetime is niave so we tell it its in UTC
-        loc_time = datetime.replace(tzinfo=from_zone)
-        
-        loc_time = loc_time.astimezone(to_zone).strftime('%I:%M:%S %p')
-        
+ 
         #Get the Sender's Name
         user_sending = message.author.name
-        
+       
+        #Get Times from Message
+        times = get_times(message)
+
+        utc_time = times[0]
+        loc_time = times[1]
+
         #Send a message to the channel
-        await message.channel.send(user_sending + ', thanks for clocking in at ' + loc_time + '!')
+        await message.channel.send(user_sending + \
+                                ', thanks for clocking in at ' + loc_time + '!')
         
         #Now we want to add the start time to the DB
         await add_clock_in(user,utc_time)
     if ('clock out' in message_low) | ('clocking out' in message_low):
         user = message.author.name
         
-        datetime = message.created_at
+        #Get  Times from Message
+        times = get_times(message)
         
-        utc_time = datetime.strftime('%H%M%S')
+        utc_time = times[0]
+        loc_time = times[1]
+        #-------------------------------
 
-        loc_time = datetime.replace(tzinfo=from_zone)
+        #Get a random motivational quote
+        indx = random.randint(0,len(Quotes))
+        motivation = Quotes[indx]
 
-        loc_time = loc_time.astimezone(to_zone).strftime('%I:%M:%S %p')
+        #Removed the newline char from the string
+        motivation = motivation[:(len(motivation)-1)]
 
-        await message.channel.send(user + ', clocking out at ' + loc_time + '?')
+        #Add times to DB and calculate PTO
+        PTO = clock_out(user,utc_time)
 
-        await clock_out(user,utc_time)
+        #Check for clocking out before clocking in
+        if((PTO[0] == 0.0) & (PTO[1] == 0.0)):
+            await message.channel.send(user + " you must clock in first.")
+        
+        else:
+            await message.channel.send(user + ', clocking out at ' + loc_time \
+                                    + ' already?\nRemember %s' % motivation \
+                                    + '!\n - From Corporate')
+            #Send PTO message
+            await message.channel.send('Earned %.5f' \
+                                        %PTO[0] + \
+                                        ' hours of PTO, %.5f total hours PTO'\
+                                        % PTO[1])
 
 client.run(TOKEN)
